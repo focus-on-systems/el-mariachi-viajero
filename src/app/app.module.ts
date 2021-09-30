c59524e1ee3191c1994d137e30c49fbde97c1af2
BenjaminGuzman <9benjaminguzman@gmail.com>
2021-09-30 13:05:54 -0500

    Removed unnecessary angular material imports in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {MatCardModule} from '@angular/material/card';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {CarouselItemComponent} from './landing/carousel-item/carousel-item.component';
 import {NavOptionComponent} from './nav/nav-option/nav-option.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import { ManageToursComponent } from './tours/manage-tours/manage-tours.component';
+import {ManageToursComponent} from './tours/manage-tours/manage-tours.component';
 import {NavComponent} from './nav/nav.component';
 import {FooterComponent} from './footer/footer.component';
-import { LandingComponent } from './landing/landing/landing.component';
-import { TourItemComponent } from './tours/tour-item/tour-item.component';
-import { TourDetailsComponent } from './tours/tour-details/tour-details.component';
+import {LandingComponent} from './landing/landing/landing.component';
+import {TourItemComponent} from './tours/tour-item/tour-item.component';
+import {TourDetailsComponent} from './tours/tour-details/tour-details.component';
 
 @NgModule({
 	declarations: [
@@ -21,15 +20,14 @@ import { TourDetailsComponent } from './tours/tour-details/tour-details.componen
 		NavComponent,
 		FooterComponent,
 		ManageToursComponent,
-  LandingComponent,
-  TourItemComponent,
-  TourDetailsComponent
+		LandingComponent,
+		TourItemComponent,
+		TourDetailsComponent
 	],
 	imports: [
 		BrowserModule,
 		BrowserAnimationsModule,
-		AppRoutingModule,
-		MatCardModule
+		AppRoutingModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
