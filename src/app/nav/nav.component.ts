89daa9b13b4ff9cbdaa37575d3603af886f6210f
BenjaminGuzman <9benjaminguzman@gmail.com>
2021-10-05 18:30:21 -0500

    Improved navigation bar and landing page

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,81 +1,15 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, HostListener, ViewChild} from '@angular/core';
-import {animate, state, style, transition, trigger} from "@angular/animations";
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input} from '@angular/core';
 
 @Component({
 	selector: 'app-nav',
 	templateUrl: './nav.component.html',
 	styleUrls: ['./nav.component.scss'],
-	animations: [
-		trigger('hideShow', [
-			state('hide', style({
-				opacity: 0,
-				height: '0px',
-				display: 'none'
-			})),
-			state('show', style({
-				opacity: 0.9,
-				height: '3rem',
-				display: 'flex'
-			})),
-			transition('hide => show', [
-				style({
-					display: 'flex'
-				}),
-				animate('500ms ease-out')
-			]),
-			transition('show => hide', [
-				animate('100ms ease-out')
-			])
-		]),
-	],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavComponent {
-	@ViewChild('nav')
-	// @ts-ignore
-	public nav: NavComponent;
-
-	/**
-	 * After this height the nav should be shown
-	 * Height is in px
-	 */
-	public minHeight2ShowNav: number = 50;
-
-	public isNavShowing: boolean = false;
-
-	public _navAnimationState: NavAnimationState = 'hide';
+	@Input()
+	public position: 'sticky' | 'fixed' = 'fixed';
 
 	constructor(private _changeDetectorRef: ChangeDetectorRef) {
 	}
-
-	@HostListener('window:scroll', [])
-	onWindowScroll() {
-		if (window.pageYOffset > this.minHeight2ShowNav) { // nav should be shown
-			if (this.isNavShowing)
-				return;
-
-			this.isNavShowing = true;
-			this.navAnimationState = 'show';
-		} else { // nav should be hidden
-			if (!this.isNavShowing)
-				return;
-
-			this.isNavShowing = false;
-			this.navAnimationState = 'hide';
-		}
-	}
-
-	set navAnimationState(state: NavAnimationState) {
-		if (state === this._navAnimationState) // do nothing if the new state is the current one
-			return;
-
-		this._navAnimationState = state;
-		this._changeDetectorRef.markForCheck();
-	}
-
-	get navAnimationState() {
-		return this._navAnimationState;
-	}
 }
-
-export type NavAnimationState = 'show' | 'hide';
