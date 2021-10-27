cc2e08b766c34f9a41bcb6a8eac60a94db66bca5
Warrior <licantroposkate@gmail.com>
2021-10-27 02:13:13 -0500

    Adding about company section and more

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {ManageToursComponent} from "./tours/manage-tours/manage-tours.component";
 import {TourDetailsComponent} from "./tours/tour-details/tour-details.component";
 import {LandingComponent} from "./landing/landing/landing.component";
+import {AboutCompanyComponent} from "./about-company/about-company.component";
 
 const routes: Routes = [
 	{path: "", redirectTo: "/inicio", pathMatch: "full"},
 	{path: "inicio", component: LandingComponent},
 	{path: "tours", component: ManageToursComponent},
+	{path: "nosotros", component: AboutCompanyComponent},
 	{path: ":tourId/tour-details", component: TourDetailsComponent},
 	{path: "**", redirectTo: "/inicio", pathMatch: "full"}, // 404
 ];
