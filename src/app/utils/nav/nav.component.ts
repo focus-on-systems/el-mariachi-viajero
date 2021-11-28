0841982f0a304320dbb1c54e24bc0d175ef2f52c
BenjaminGuzman <9benjaminguzman@gmail.com>
2021-11-27 23:47:05 -0600

    Enhancement to responsiveness of navigation bar in small screens Enhancements to contact form Migration to material icons

--- src/app/utils/nav/nav.component.ts
+++ src/app/utils/nav/nav.component.ts
@@ -1,12 +1,31 @@
 import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, Input, OnInit, PLATFORM_ID} from '@angular/core';
 import {CONTACT_EMAIL, CONTACT_PHONE_NUMBER} from '../../globals';
 import {isPlatformBrowser} from "@angular/common";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
 	selector: 'app-nav',
 	templateUrl: './nav.component.html',
 	styleUrls: ['./nav.component.scss'],
-	changeDetection: ChangeDetectionStrategy.OnPush
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	animations: [
+		trigger('smallOpenClose', [
+			state('open', style({
+				height: 'calc(100vh - 3.5rem)',
+				opacity: 1
+			})),
+			state('closed', style({
+				height: '0px',
+				opacity: 0
+			})),
+			transition('open => closed', [
+				animate('250ms ease-in')
+			]),
+			transition('closed => open', [
+				animate('200ms ease-out')
+			])
+		])
+	]
 })
 export class NavComponent implements OnInit {
 	public readonly CONTACT_PHONE_NUMBER = CONTACT_PHONE_NUMBER;
@@ -19,6 +38,8 @@ export class NavComponent implements OnInit {
 	 * Tells if the device is connected to the internet
 	 */
 	public isOnline: boolean = true;
+
+	public isSmallNavOpen: boolean = false;
 
 	constructor(private _changeDetectorRef: ChangeDetectorRef, @Inject(PLATFORM_ID) private _platformId: Object) {
 	}
