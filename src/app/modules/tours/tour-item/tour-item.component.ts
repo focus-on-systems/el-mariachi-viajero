e5cc6073750346ef9f0558bda06997113ee99caa
Benjamín Guzmán <bg@benjaminguzman.net>
2022-02-15 21:31:48 -0600

    Added proposal to show place and categories in tour card. Refactored some code

--- src/app/modules/tours/tour-item/tour-item.component.ts
+++ src/app/modules/tours/tour-item/tour-item.component.ts
@@ -14,6 +14,8 @@ export class TourItemComponent implements OnInit {
   public bgImageStyle: string = "";
   public link: string = "";
 
+  public tourCategories: TourCategory[] = [{name: "Aventura", id: "hdsiohfiosd"}, {name: "Romance", id: "hsdifohsd"}, {name: "Cultural", id: "dhfiosdhoiwe"}];
+
   constructor(private _apollo: Apollo, private _changeDetectorRef: ChangeDetectorRef) { }
 
   ngOnInit(): void {
@@ -31,4 +33,12 @@ export class TourItemComponent implements OnInit {
     return this._data;
   }
 
+}
+
+/**
+ * TODO: moverlo de aquí y meter los campos correctos
+ */
+interface TourCategory {
+  name: string;
+  id: string;
 }
